46dd795fa59c1ada3e5ab1f86d713ebc5211f5ac
Sandeep Jagan <96875555+sandy25x@users.noreply.github.com>
2024-06-27 01:03:22 +0530

    Create witchframes.py
    This code converts a gif file to frames of png file.

---
+++ witchframes.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import os
+
+def extract_frames(gif_path, output_folder):
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+        
+    with Image.open(gif_path) as img:
+        for frame in range(img.n_frames):
+            img.seek(frame)
+            img.save(os.path.join(output_folder, f"witch{frame}.png"))
+
+# Provide the correct path to the GIF file
+gif_path = r'C:\Users\minij\Downloads\knight phobia\witch.gif'
+output_folder = r'C:\Users\minij\Downloads\knight phobia\witch_frames'
+
+# Extract frames from witch.gif and save them to the "witch_frames" folder
+extract_frames(gif_path, output_folder)
