dbf834bb3613baf86b4464ddb99fb3772e6f296e
Sandeep Jagan <96875555+sandy25x@users.noreply.github.com>
2024-06-27 01:31:33 +0530

    Update witchframes.py

--- witchframes.py
+++ witchframes.py
@@ -12,8 +12,8 @@ def extract_frames(gif_path, output_folder):
             img.save(os.path.join(output_folder, f"witch{frame}.png"))
 
 # Provide the correct path to the GIF file
-gif_path = r'C:\Users\minij\Downloads\knight phobia\witch.gif'
-output_folder = r'C:\Users\minij\Downloads\knight phobia\witch_frames'
+gif_path = r'C:\Users\yourpc\Downloads\knight phobia\witch.gif'
+output_folder = r'C:\Users\yourpc\Downloads\knight phobia\witch_frames'
 
 # Extract frames from witch.gif and save them to the "witch_frames" folder
 extract_frames(gif_path, output_folder)
